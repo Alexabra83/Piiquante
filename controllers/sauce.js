b3c696001d5ecd5e1b563135fc8317ee6b7caa8e
Alexabra83 <alexabra83@gmail.com>
2023-11-28 12:23:36 +0100

    ajustement mise en forme du code

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -27,64 +27,64 @@ exports.createSauce = (req, res, next) => {
   delete sauceObject._id;
   delete sauceObject._userId;
   const sauce = new Sauce({
-      ...sauceObject,
-      userId: req.auth.userId,
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    ...sauceObject,
+    userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
 
   sauce.save()
-  .then(() => { res.status(201).json({message: 'Sauce enregistré !'})})
-  .catch(error => { res.status(400).json( { error })})
+    .then(() => { res.status(201).json({ message: 'Sauce enregistré !' }) })
+    .catch(error => { res.status(400).json({ error }) })
 };
 
 // Modifier une sauce, uniquement autorisé par l'utilisateur qui l'a créée
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ? {
-      ...JSON.parse(req.body.sauce),
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   } : { ...req.body };
   delete sauceObject._userId;
-  Sauce.findOne({_id: req.params.id})
-      .then((sauce) => {
-          if (sauce.userId != req.auth.userId) {
-              res.status(403).json({ message : 'Not authorized'});
-          } else if(req.file){
-            const filename = sauce.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-              Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
-              .then(() => res.status(200).json({message : 'Objet modifié!'}))
-              .catch(error => res.status(400).json({ error }));
-            });
-          } else {
-              Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
-              .then(() => res.status(200).json({message : 'Objet modifié!'}))
-              .catch(error => res.status(400).json({ error }));
-          }
-      })
-      .catch((error) => {
-        console.log(error);
-          res.status(400).json({ error });
-      });
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(403).json({ message: 'Not authorized' });
+      } else if (req.file) {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+            .catch(error => res.status(400).json({ error }));
+        });
+      } else {
+        Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+          .catch(error => res.status(400).json({ error }));
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json({ error });
+    });
 };
 
 // Supprimer une sauce
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id})
-      .then(sauce => {
-          if (sauce.userId != req.auth.userId) {
-              res.status(403).json({message: 'Not authorized'});
-          } else {
-              const filename = sauce.imageUrl.split('/images/')[1];
-              fs.unlink(`images/${filename}`, () => {
-                  Sauce.deleteOne({_id: req.params.id})
-                      .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
-                      .catch(error => res.status(403).json({ error }));
-              });
-          }
-      })
-      .catch( error => {
-          res.status(500).json({ error });
-      });
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(403).json({ message: 'Not authorized' });
+      } else {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+            .then(() => { res.status(200).json({ message: 'Objet supprimé !' }) })
+            .catch(error => res.status(403).json({ error }));
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 };
 
 // Gère les Likes et Dislikes des sauces
@@ -93,57 +93,57 @@ exports.likeDislikeSauce = (req, res, next) => {
   const like = req.body.like;
   const sauceId = req.params.id;
   //Test id utilisateur
-  if (req.body.userId !== req.auth.userId){
-   return res.status(403).json({ message : 'Not authorized'});
+  if (req.body.userId !== req.auth.userId) {
+    return res.status(403).json({ message: 'Not authorized' });
   }
-  if (like != -1 && like !=1 && like != 0){
-    return res.status(400).json({ message : "unknown action bad 'like' value"});
+  if (like != -1 && like != 1 && like != 0) {
+    return res.status(400).json({ message: "unknown action bad 'like' value" });
   }
-  Sauce.findOne({_id: sauceId})
-      .then((sauce) => {
-        let newDislikesValue = sauce.dislikes;
-        let newLikesValue = sauce.likes;
-        let newUsersLikedValue = sauce.usersLiked;
-        let newUsersDislikedValue = sauce.usersDisliked;
-        if (like == -1){    //Empèche l'utilisateur de dislike plusieurs fois
-          const userIndex = sauce.usersDisliked.indexOf(userId)
-          if (userIndex > -1){
-            return res.status(400).json({ message : "can't dislike twice"});
-          }
-          newUsersDislikedValue.push(userId);    //Met un dislike si pas déjà fait
-          newDislikesValue++;
-        }else if (like == 1){    //Empèche l'utilisateur de like plusieurs fois
-          const userIndex = sauce.usersLiked.indexOf(userId)
-          if (userIndex > -1){
-            return res.status(400).json({ message : "can't like twice"});
-          }
-          newUsersLikedValue.push(userId);    //Met un like si pas déjà fait
-          newLikesValue++;
-        }else {
-          const userDislikeIndex = sauce.usersDisliked.indexOf(userId);
-          const userLikeIndex = sauce.usersLiked.indexOf(userId);
-          if (userDislikeIndex > -1){    // Permet de retirer un dislike
-            newUsersDislikedValue.splice(userDislikeIndex, 1);
-            newDislikesValue--;
-          }else if(userLikeIndex > -1){    //Permet de retirer un like
-            newUsersLikedValue.splice(userLikeIndex, 1);
-            newLikesValue--;
-          }else {
-            return res.status(400).json({ message : "can't remove dislike or like that don't exist"});
-          }
+  Sauce.findOne({ _id: sauceId })
+    .then((sauce) => {
+      let newDislikesValue = sauce.dislikes;
+      let newLikesValue = sauce.likes;
+      let newUsersLikedValue = sauce.usersLiked;
+      let newUsersDislikedValue = sauce.usersDisliked;
+      if (like == -1) {    //Empèche l'utilisateur de dislike plusieurs fois
+        const userIndex = sauce.usersDisliked.indexOf(userId)
+        if (userIndex > -1) {
+          return res.status(400).json({ message: "can't dislike twice" });
         }
-        
-        Sauce.updateOne({ _id: sauceId}, {    //Met à jour les informations de la sauce like/dislike
-          likes: newLikesValue,
-          dislikes: newDislikesValue,
-          usersDisliked: newUsersDislikedValue,
-          usersLiked: newUsersLikedValue
-        })
-              .then(() => res.status(200).json({message : 'Objet modifié!'}))
-              .catch(error => res.status(400).json({ error }));
+        newUsersDislikedValue.push(userId);    //Met un dislike si pas déjà fait
+        newDislikesValue++;
+      } else if (like == 1) {    //Empèche l'utilisateur de like plusieurs fois
+        const userIndex = sauce.usersLiked.indexOf(userId)
+        if (userIndex > -1) {
+          return res.status(400).json({ message: "can't like twice" });
+        }
+        newUsersLikedValue.push(userId);    //Met un like si pas déjà fait
+        newLikesValue++;
+      } else {
+        const userDislikeIndex = sauce.usersDisliked.indexOf(userId);
+        const userLikeIndex = sauce.usersLiked.indexOf(userId);
+        if (userDislikeIndex > -1) {    // Permet de retirer un dislike
+          newUsersDislikedValue.splice(userDislikeIndex, 1);
+          newDislikesValue--;
+        } else if (userLikeIndex > -1) {    //Permet de retirer un like
+          newUsersLikedValue.splice(userLikeIndex, 1);
+          newLikesValue--;
+        } else {
+          return res.status(400).json({ message: "can't remove dislike or like that don't exist" });
+        }
+      }
+
+      Sauce.updateOne({ _id: sauceId }, {    //Met à jour les informations de la sauce like/dislike
+        likes: newLikesValue,
+        dislikes: newDislikesValue,
+        usersDisliked: newUsersDislikedValue,
+        usersLiked: newUsersLikedValue
       })
-      .catch((error) => {
-        console.log(error);
-          res.status(400).json({ error });
-      });
+        .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json({ error });
+    });
 };
--- controllers/user.js
+++ controllers/user.js
@@ -4,42 +4,42 @@ const User = require('../models/User');
 
 // Crée un compte utilisateur
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)    //Crypte le mot de passe
-    .then(hash => {    //créer l'utilisateur en model user
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()    //Créer le nouvel utilisateur
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+    bcrypt.hash(req.body.password, 10)    //Crypte le mot de passe
+        .then(hash => {    //créer l'utilisateur en model user
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()    //Créer le nouvel utilisateur
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 
 //Connection au compte de l'utilisateur
 exports.login = (req, res, next) => {    //Cherche si l'utilisateur est déjà existant
-  User.findOne({ email: req.body.email })
-      .then(user => {
-          if (!user) {    //Utilisateur non trouvé
-              return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-          }
-          bcrypt.compare(req.body.password, user.password)
-              .then(valid => {
-                  if (!valid) {    //En cas de mauvais mot de passe
-                      return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                  }
-                  res.status(200).json({    //L'utilisateur a été trouvé
-                      userId: user._id,
-                      token: jwt.sign(
-                          { userId: user._id,},
-                          process.env.JWT_KEY,    //Asigne un token qui donne accés aux autres routes
-                          { expiresIn: '24h' }
-                      )
-                  });
-              })
-              .catch(error => res.status(500).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {    //Utilisateur non trouvé
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {    //En cas de mauvais mot de passe
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    }
+                    res.status(200).json({    //L'utilisateur a été trouvé
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id, },
+                            process.env.JWT_KEY,    //Asigne un token qui donne accés aux autres routes
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
